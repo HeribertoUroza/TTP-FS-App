da83afe201db365eb681d8b2e0320c3f4a64cf98
Heriberto Uroza <heribertouroza@pursuit.org>
2020-02-22 20:22:57 -0500

    created db connection to postgres

---
+++ backend/services/UserS.js
@@ -0,0 +1,7 @@
+// ----LOCAL MODULES
+const { getDbConnection } = require('./dbConnection/dbConnection');
+const { dbAddress } = require('./dbConnection/dbAddress');
+
+// ----USER SERVICE FUNCTIONS
+
+    
