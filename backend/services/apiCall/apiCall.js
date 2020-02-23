fedfd0412281efe67f642d5936f619a378aba0bf
Heriberto Uroza <heribertouroza@pursuit.org>
2020-02-23 15:16:25 -0500

    npm installed axios to get api data. currently working

--- backend/services/apiCall/apiCall.js
+++ backend/services/apiCall/apiCall.js
@@ -1,9 +1,22 @@
 // ----API CALL
 const { api } = require('./config');
 
+// ----NPM MODULE
+const axios = require('axios');
+
 const getApiData = ticker => {
-    //return `${api.liveUrl}${ticker}${api.liveKey}`
-    return `${api.testUrl}${ticker}${api.testKey}`
+    let data = axios.get(`${api.testUrl}${ticker}${api.testKey}`)
+                    .then( response => {
+                        response.data
+                        return response.data
+                    })
+                    .catch( error => {
+                        return error.toString()
+                    })
+
+    //axios.get(`${api.liveUrl}${ticker}${api.liveKey}`)
+    return data;
+
 }
 
-console.log(getApiData('fb'))
+export default getApiData;
