66108db5f4dab9beb90cba849b8ea1397e4fc4b5
Heriberto Uroza <heribertouroza@pursuit.org>
2020-02-25 21:28:12 -0500

    fixed props error message

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -6,7 +6,7 @@ function NavBar(props) {
     const notUser = <nav className='nav-notuser'>
         <h1 className='nav-title'>TTP-FS-App</h1>
         <form className='login-section' >
-            <h3 className='nav-subtitle'>{props.error ? props.errpr : `Coming Back? Login back here!`}</h3>
+            <h3 className='nav-subtitle'>{props.error ? props.error : `Coming Back? Login back here!`}</h3>
             <input className='l-email' type='email' placeholder='Email' autoComplete='on' name='nav_email' onChange={props.onChange} ></input>
             <input className='l-password' type='password' placeholder='Password' autoComplete='on' name='nav_password' onChange={props.onChange} ></input>
             <button className='login' onClick={props.onClick}>Login</button>
