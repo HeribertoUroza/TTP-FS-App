6bfd9a0bbb8de7b292741f1866db9d4c54ac0825
Heriberto Uroza <heribertouroza@pursuit.org>
2020-02-26 04:53:04 -0500

    added navbar to transaction page. Nav bar is now more dynamic

--- frontend/src/containers/TransactionPage/TransactionPage.js
+++ frontend/src/containers/TransactionPage/TransactionPage.js
@@ -1,17 +1,65 @@
 import React from 'react';
+import firebase from '../../firebase/config';
+
+// ----CONTEXT
+import AuthContext from '../../context/AuthContext';
+
+// ----COMPONENT
+import NavBar from '../../components/NavBar';
+
 
 class TransactionPage extends React.Component {
     constructor(props) {
         super(props)
 
-        this.state = {};
+        this.state = {
+            user: null,
+        };
+    }
+
+    componentDidMount() {
+        this.unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({
+                    user: user,
+                    userEmail: user.email,
+                })
+            } else {
+                this.setState({
+                    user: null
+                })
+            }
+        })
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    handleLogOut = (e) => {
+        e.preventDefault();
+
+        firebase.auth().signOut();
+
+        this.props.history.push('/');
     }
 
     render(){
+    console.log(this.props.history.location.pathname)
         return(
-            <div className='test3'>
-                TransactionPage
-                </div>
+            <AuthContext.Consumer>
+                {
+                    (user) => {
+                        if(user) {
+                            return (
+                                <>
+                                    <NavBar user={user} full_name={this.state.full_name} onClick={this.handleLogOut} path={this.props.history.location.pathname}></NavBar>
+                                </>
+                            )
+                        }
+                    }
+                }
+            </AuthContext.Consumer>
         )
     }
 }
