e1197de31a042c99328c29517e158e407c416fe9
Heriberto Uroza <heribertouroza@pursuit.org>
2020-02-26 10:27:36 -0500

    added transaction page, added styling as well

--- frontend/src/containers/TransactionPage/TransactionPage.js
+++ frontend/src/containers/TransactionPage/TransactionPage.js
@@ -70,6 +70,19 @@ class TransactionPage extends React.Component {
                                 <>
                                     <NavBar user={user} full_name={this.state.full_name} onClick={this.handleLogOut} path={this.props.history.location.pathname}></NavBar>
 
+                                    <section className='tranContainer'>
+                                        <h1 className='tranTitle' >Here Are All Of Your Transactions</h1>
+
+                                        <h3 className='p-title-h3'><span className='title-s'>NAME</span><span className='title-s' >TICKER</span><span className='title-s' >QUANTITY</span><span className='title-s' >AMOUNT</span><span className='title-s' >STATUS</span></h3>
+                                        <ul className='tranListContainer'>
+                                            {
+                                                this.state.data ? this.state.data.map(( e, i) => {
+                                                    return <li className='tranListItem' key={i}><span className='info'>{e.name}</span><span className='info' >{e.ticker}</span><span className='info' >{e.quantity}</span><span className='info' >${e.amount}</span><span className='info'>{e.status}</span></li>
+                                                }) : <h1 className='tranTitle'>No Transactions Yet</h1>
+                                            }
+                                        </ul>
+                                    </section>
+
                                 </>
                             )
                         }
