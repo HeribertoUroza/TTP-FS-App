92ec9019aa7d37d04de083c8efd326fa973b1240
Heriberto Uroza <heribertouroza@pursuit.org>
2020-03-08 11:30:00 -0400

    updated config file

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -2,16 +2,15 @@ import { firebase } from '@firebase/app';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCyVGu7gWTSCJHTmmZJ5_9BcQGiHFcRbD8",
-    authDomain: "ttp-fs-app.firebaseapp.com",
-    databaseURL: "https://ttp-fs-app.firebaseio.com",
-    projectId: "ttp-fs-app",
-    storageBucket: "ttp-fs-app.appspot.com",
-    messagingSenderId: "368063808790",
-    appId: "1:368063808790:web:896e2fb691eb8ea34d3c37",
-    measurementId: "G-NTZEDXRNBK"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    databaseURL: process.env.REACT_APP_databaseURL,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_messagingSenderId
 };
-
 
 firebase.initializeApp(firebaseConfig);
 
