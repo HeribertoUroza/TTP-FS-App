2b904cfa17da2f45e9995ea402bcd192cb99c56e
Heriberto Uroza <heribertouroza@pursuit.org>
2020-03-19 22:36:22 -0400

    proccess.env not working, tetsting out with actual key

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -2,14 +2,14 @@ import { firebase } from '@firebase/app';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    databaseURL: process.env.REACT_APP_databaseURL,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
-    measurementId: process.env.REACT_APP_messagingSenderId
+    apiKey: 'AIzaSyCyVGu7gWTSCJHTmmZJ5_9BcQGiHFcRbD8',
+    authDomain: 'ttp-fs-app.firebaseapp.com',
+    databaseURL: 'https://ttp-fs-app.firebaseio.com',
+    projectId: 'ttp-fs-app',
+    storageBucket: 'ttp-fs-app.appspot.com',
+    messagingSenderId: '368063808790',
+    appId: '1:368063808790:web:896e2fb691eb8ea34d3c37',
+    measurementId: 'G-NTZEDXRNBK'
 };
 
 firebase.initializeApp(firebaseConfig);
